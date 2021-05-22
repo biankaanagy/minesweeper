95e90dee9398983b865b95c4f96728a24f2f76ed
biankaanagy <bianka.nagy2000@gmail.com>
2021-05-22 18:04:11 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+import javafx.application.Application;
+import main.MyApplication;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MyApplication.class, args);
+    }
+}
