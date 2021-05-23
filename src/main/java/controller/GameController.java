17bb8e29c8830ea402c0787db64eee88e30461e6
biankaanagy <bianka.nagy2000@gmail.com>
2021-05-23 20:14:51 +0200

    Started the controllers

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,47 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+import util.javafx.ControllerHelper;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
 public class GameController {
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button giveUpFinishButton;
+
+    public void handleResetButton(ActionEvent actionEvent)  {
+        Logger.debug("{} is pressed", ((Button) actionEvent.getSource()).getText());
+        Logger.info("Resetting game");
+        //resetGame();
+    }
+
+    public void handleGiveUpFinishButton(javafx.event.ActionEvent actionEvent) throws IOException {
+        var buttonText = ((Button) actionEvent.getSource()).getText();
+        Logger.debug("{} is pressed", buttonText);
+        if (buttonText.equals("Give Up")) {
+            //stopwatch.stop();
+            Logger.info("The game has been given up");
+        }
+        Logger.debug("Saving result");
+        //gameResultDao.persist(createGameResult());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        ControllerHelper.loadAndShowFXML(fxmlLoader, "/fxml/highscores.fxml", stage);
+        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/highscores.fxml"));
+    }
+
 }
--- src/main/java/controller/OpeningController.java
+++ src/main/java/controller/OpeningController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,18 +8,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.tinylog.Logger;
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.Label;
 import java.io.IOException;
 
 public class OpeningController {
 
     @FXML
-    public void startAction(javafx.event.ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
-        Parent root = fxmlLoader.load();
-        fxmlLoader.<GameController>getController();
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+    private TextField playerNameTextField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    public void startAction(ActionEvent actionEvent) throws IOException {
+        if (playerNameTextField.getText().isEmpty()) {
+            errorLabel.setText("Please enter a name!");
+        } else{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+            Parent root = fxmlLoader.load();
+            fxmlLoader.<GameController>getController();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+            Logger.info("The user's name is set to {}, loading game scene", playerNameTextField.getText());
+        }
     }
+
 
 }
